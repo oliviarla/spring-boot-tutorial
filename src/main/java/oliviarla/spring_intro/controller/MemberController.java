ab4c74a5837b656605c24b20f47b85ade1e71356
minkyeong <alsrud713@gmail.com>
2021-01-08 22:09:33 +0900

    add MVC files

--- src/main/java/oliviarla/spring_intro/controller/MemberController.java
+++ src/main/java/oliviarla/spring_intro/controller/MemberController.java
@@ -1,8 +1,14 @@
 package oliviarla.spring_intro.controller;
 
+import oliviarla.spring_intro.domain.Member;
 import oliviarla.spring_intro.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller //스프링 컨테이너에서 스프링 빈으로 controller를 관리하도록 함
 public class MemberController {
@@ -17,4 +23,27 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; //홈 화면으로 이동
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
---
+++ src/main/java/oliviarla/spring_intro/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package oliviarla.spring_intro.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
